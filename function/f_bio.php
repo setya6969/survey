80b377e9f1eb60daffb9ab11f9fe7b1a1c4b4d80
setya6969 <rahadisetya69@gmail.com>
2021-05-25 22:02:47 +0700

    error handling

--- function/f_bio.php
+++ function/f_bio.php
@@ -21,17 +21,21 @@
     $ipk = $mysqli->real_escape_string($_POST['ipk']);
     $domisili = $mysqli->real_escape_string($_POST['domisili']);
     $activity = $mysqli->real_escape_string($_POST['activity']);
-    
+
     $query = "INSERT INTO biodata_mahasiswa (`user_id`, `name`,`email`,`prodi`,`gender`,`angkatan`,`phone`, `ipk`, `domisili`, `activity`) VALUES(".
     $values .= "'$user_id', '$name', '$email', '$prodi', '$gender', '$angkatan', '$phone', '$ipk', '$domisili', '$activity')";
 
-    if($mysqli->query($query))
-    {
+    if($mysqli->query($query)){
         header("location : ".BASE_URL."angket");
-        exit();
-    }
-    else
-    {
-        echo "failed " . $mysqli->error;
+        exit();                   
+    }else{
+       if($mysqli->errno($conn) == 1062)
+       {
+            echo "duplicate entry no need to insert into DB";
+       }    
+       else
+       {
+            echo "failed " . $mysqli->error;     
+       }
     }
 ?>
